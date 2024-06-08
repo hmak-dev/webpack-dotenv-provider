13403c0a05e6da3200cdbfc809e39e1e8b42f8f3
Hossein Maktoobian <8bitsight@gmail.com>
2024-06-08 15:19:24 +0330

    feat: allow system env variables with "ENV_" prefix

--- index.js
+++ index.js
@@ -4,10 +4,17 @@ const dotenv = require('dotenv');
 module.exports = ({ path }) => {
 	const { parsed } = dotenv.config({ path });
 
-	return new webpack.DefinePlugin(
-		Object.keys(parsed).reduce((output, key) => {
-			output[`process.env.${key}`] = `'${parsed[key]}'`;
-			return output;
-		}, {})
-	);
+	const envVariables = {};
+
+	Object.keys(parsed).forEach((key) => {
+		envVariables[`process.env.${key}`] = `'${parsed[key]}'`;
+	});
+
+	Object.keys(process.env).forEach((key) => {
+		if (key.startsWith('ENV_')) {
+			envVariables[`process.env.${key.replace(/^ENV_/, '')}`] = `'${process.env[key]}'`;
+		}
+	});
+
+	return new webpack.DefinePlugin(envVariables);
 };
